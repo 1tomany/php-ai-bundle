9ae6c73a3da13379fdd4021e56167d3ef5386133
Vic Cherubini <vcherubini@gmail.com>
2026-01-29 14:47:44 -0600

    wip

---
+++ README.md
@@ -0,0 +1,19 @@
+# PHP AI Bundle for Symfony
+
+This package wraps the `1tomany/php-ai` library into an easy to use Symfony bundle.
+
+## Installation
+
+Install the bundle using Composer:
+
+```
+composer require 1tomany/php-ai-bundle
+```
+
+## Credits
+
+- [Vic Cherubini](https://github.com/viccherubini), [1:N Labs, LLC](https://1tomany.com)
+
+## License
+
+The MIT License
---
+++ src/AiBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace OneToMany\DataUriBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class AiBundle extends AbstractBundle
+{
+    /**
+     * @param array<string, mixed> $config
+     */
+    public function loadExtension(array $config, ContainerConfigurator $configurator, ContainerBuilder $container): void
+    {
+        $configurator->import('../config/services.yaml');
+    }
+}
