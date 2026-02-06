e03c839095fbdc466f5a0c8bc8f9071ed966c2c6
Vic Cherubini <vcherubini@gmail.com>
2026-02-06 15:08:11 -0600

    wip

--- src/AiBundle.php
+++ src/AiBundle.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace OneToMany\DataUriBundle;
+namespace OneToMany\AiBundle;
 
+use OneToMany\AI\Contract\Client\FileClientInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
@@ -11,8 +12,12 @@ class AiBundle extends AbstractBundle
     /**
      * @param array<string, mixed> $config
      */
-    public function loadExtension(array $config, ContainerConfigurator $configurator, ContainerBuilder $container): void
+    public function loadExtension(array $config, ContainerConfigurator $configurator, ContainerBuilder $builder): void
     {
+        $builder
+            ->registerForAutoconfiguration(FileClientInterface::class)
+            ->addTag('1tomany.ai.clients.file_client');
+
         $configurator->import('../config/services.yaml');
     }
 }
