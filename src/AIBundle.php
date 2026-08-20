a9ca1a2accef524281cbe68f9b59d65ed003126b
Vic Cherubini <vcherubini@gmail.com>
2026-08-20 13:38:06 -0500

    wip

--- src/AIBundle.php
+++ src/AIBundle.php
@@ -116,7 +116,7 @@ class AIBundle extends AbstractBundle
         $services
             ->set(self::TRANSPORT_SERVICE, Transport::class)
                 ->arg('$httpClient', service($config['transport']['http_client']))
-                ->arg('$denormalizer', service('serializer'))
+                ->arg('$serializer', service('serializer'))
 
             ->set(self::FILES_SERVICE, Files::class)
                 ->arg('$providers', tagged_iterator(self::FILE_PROVIDER_TAG))
@@ -137,18 +137,18 @@ class AIBundle extends AbstractBundle
         if (isset($config['gemini'])) {
             $services
                 ->set(self::GEMINI_NORMALIZER_SERVICE, GeminiQueryNormalizer::class)
-                    ->tag('serializer.normalizer', ['priority' => 100])
+                    ->tag('serializer.normalizer')
 
                 ->set(self::GEMINI_FILE_PROVIDER_SERVICE, GeminiFileProvider::class)
                     ->arg('$transport', service(self::TRANSPORT_SERVICE))
-                    ->arg('$normalizer', service('serializer'))
+                    ->arg('$serializer', service('serializer'))
                     ->arg('$apiKey', $config['gemini']['api_key'])
                     ->arg('$apiVersion', $config['gemini']['api_version'])
                     ->tag(self::FILE_PROVIDER_TAG)
 
                 ->set(self::GEMINI_QUERY_PROVIDER_SERVICE, GeminiQueryProvider::class)
                     ->arg('$transport', service(self::TRANSPORT_SERVICE))
-                    ->arg('$normalizer', service('serializer'))
+                    ->arg('$serializer', service('serializer'))
                     ->arg('$apiKey', $config['gemini']['api_key'])
                     ->arg('$apiVersion', $config['gemini']['api_version'])
                     ->tag(self::QUERY_PROVIDER_TAG)
@@ -158,18 +158,18 @@ class AIBundle extends AbstractBundle
         if (isset($config['openai'])) {
             $services
                 ->set(self::OPENAI_NORMALIZER_SERVICE, OpenAIQueryNormalizer::class)
-                    ->tag('serializer.normalizer', ['priority' => 100])
+                    ->tag('serializer.normalizer')
 
                 ->set(self::OPENAI_FILE_PROVIDER_SERVICE, OpenAIFileProvider::class)
                     ->arg('$transport', service(self::TRANSPORT_SERVICE))
-                    ->arg('$normalizer', service('serializer'))
+                    ->arg('$serializer', service('serializer'))
                     ->arg('$apiKey', $config['openai']['api_key'])
                     ->arg('$apiVersion', $config['openai']['api_version'])
                     ->tag(self::FILE_PROVIDER_TAG)
 
                 ->set(self::OPENAI_QUERY_PROVIDER_SERVICE, OpenAIQueryProvider::class)
                     ->arg('$transport', service(self::TRANSPORT_SERVICE))
-                    ->arg('$normalizer', service('serializer'))
+                    ->arg('$serializer', service('serializer'))
                     ->arg('$apiKey', $config['openai']['api_key'])
                     ->arg('$apiVersion', $config['openai']['api_version'])
                     ->tag(self::QUERY_PROVIDER_TAG)
